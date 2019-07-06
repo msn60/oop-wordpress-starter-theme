3ad160a7058622896f7d055d77548defeb9acaca
Mehdi <msoli666@gmail.com>
2019-07-06 09:42:58 +0430

    Add DocBlock in header and footer

--- header.php
+++ header.php
@@ -1,4 +1,15 @@
-<!doctype html>
+<?php
+/**
+ * The template file to use header in WordPress
+ *
+ * If you want to separate header part in your site, you can use from this template file.
+ *
+ * @package
+ * @version    1.0.1
+ * @author     Mehdi Soltani <soltani.n.mehdi@gmail.com>
+ * @link       https://wpwebmaster.ir
+ */
+?><!doctype html>
 <html lang="en">
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
